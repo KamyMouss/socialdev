4fb90a67e5ffc20890098ec760141efcca26889b
Kamy Moussavi <32441857+ogKamy@users.noreply.github.com>
2019-04-05 13:53:44 -0400

    Add secure user authentication

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const passport = require("passport");
 
 // Import apis
 const users = require("./routes/api/users");
@@ -26,7 +27,11 @@ mongoose
   .then(() => console.log("MongoDb Connected"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello world"));
+// Passport middleware used for authentication
+app.use(passport.initialize());
+
+// Passport config
+require("./config/passport")(passport);
 
 // Use routes
 app.use("/api/users", users);
